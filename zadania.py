217f0712e35eb1dbf115e99ea4646e4c3d96990f
unknown <hedrzak.marcin@gmail.com>
2019-06-18 19:25:27 +0200

    zadania od tatay

--- zadania.py
+++ zadania.py
@@ -1,3 +1,48 @@
-cd 'C:\Users\Hemmar\Documents\z ania'
-cd .\python\
-    
+zmiennaherbata=9
+zmiennadugopis=4
+zmiennamis=2
+
+
+print(zmiennadugopis+zmiennamis)
+print(zmiennadugopis-zmiennamis)
+print(zmiennaherbata*zmiennadugopis)
+print(zmiennaherbata/zmiennadugopis)
+print(zmiennaherbata % zmiennadugopis)
+
+
+b='bulka'
+f='francuska'
+print(b+' '+f)
+f='flaga'
+p='poalska'
+print(f+' '+p)
+
+
+
+
+AVENGERS=[
+'spiderman'
+'czarna wdowa'
+'kapitan MARVEL'
+'gamora'
+'groot'
+
+
+
+
+
+złoczyńcy MARVELA=[
+    'loki'
+    'thanos'
+    'ultron'
+    'abominacja'
+    'vulture'
+]
+
+]
+
+
+
+
+
+
